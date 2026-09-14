57284c4c5d1dbbb384f95fad1fa0aa78b46e8028
codeWithMohinur <mohinurrahaman700@gmail.com>
2026-09-14 11:41:21 +0530

    api error handle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`server is running on this port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo Db connection failed " , error);
+})
