241271cf9b44151171bcc2e6dc630e8c4a525f06
codeWithMohinur <mohinurrahaman700@gmail.com>
2026-09-17 02:19:24 +0530

    fixed some error

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,27 +1,26 @@
-import { v2 as cloudinary } from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
+import { log } from "console";
 import fs from "fs";
 
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadCloudinary = async(localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
-        //upload file in cloudinary
+        if(!localFilePath) return null;
+
+        //upload file
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // File uploaded
-        console.log("File is uploaded ", response.url);
-        console.log(response.url)
+        console.log("File successfully uploaded on cloudinary ", response);
         return response;
     } catch (error) {
-        fs.unlink(localFilePath) // removed the locally saved temporary file as the upload operation file got filed
-        return null
+        fs.unlinkSync(localFilePath) // removed the locally saved temporary file as the upload operation file got filed
     }
 }
 
-export {uploadCloudinary}
+export {uploadOnCloudinary}
